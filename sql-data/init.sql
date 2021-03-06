c18f2ba62188394790ad4c787dfae20ab4fa8801
rinne_grid <Rinne.Grids@gmail.com>
2021-03-06 21:34:29 +0900

    UPD circleci config.yml

--- sql-data/init.sql
+++ sql-data/init.sql
@@ -1,2 +1,4 @@
 DROP SCHEMA IF EXISTS rngd_ss_todo;
 CREATE DATABASE rngd_ss_todo CHARACTER SET utf8mb4;
+create user 'springuser'@'%' identified by 'springuser';
+grant all on my_spring_boot.* to 'springuser'@'%';
